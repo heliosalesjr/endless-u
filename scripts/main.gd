fc4355686b9df70376866d9f506be7402a00cf6a
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-09-16 16:47:52 -0300

    assets and all

--- scripts/main.gd
+++ scripts/main.gd
@@ -14,10 +14,3 @@ func _ready() -> void:
 func _process(delta: float) -> void:  
 	if not is_instance_valid(player) or not is_instance_valid(camera):
 		return
-
-	# mover câmera apenas se o player subir
-	if player.global_position.y < camera.global_position.y:
-		camera.global_position.y = lerp(camera.global_position.y, player.global_position.y, CAMERA_LERP_UP)
-
-	# mover câmera no eixo X suavemente
-	camera.global_position.x = lerp(camera.global_position.x, player.global_position.x, CAMERA_LERP_SIDE)
